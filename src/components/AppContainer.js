3c4e07bcd7340d0f33e139f8984b932692f5c841
Travis Turner <RoboEnigma@gmail.com>
2020-06-27 17:33:35 +0300

    Importing Modules Task 2 Post

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-
 function AppContainer() {
   return (
       <>
         <p>Energy Consumed:</p>
-      <>
+      </>
   );
 }
   
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-
 function Header() {
   return (
-  
+    <div>
+      <h1>Calorie-Zen</h1>
+    </div>
   );
 }
   
