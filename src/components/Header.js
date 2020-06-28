66650d0ba953fb8e3e4885342f7dc78bb02081c4
Travis Turner <RoboEnigma@gmail.com>
2020-06-28 12:21:33 +0300

    CRA Images Task 1 Post

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import headerLogo from '../images/calorieZen.png';
 
 function Header() {
   return (
     <div>
+      <img src={headerLogo} />
       <h1>Calorie-Zen</h1>
     </div>
   );
